311b34918a4ab37b4295f736b7abcfb083e6c0ac
misonikomipan <yusaku1107@icloud.com>
2023-05-06 11:37:45 +0900

    [update] parsing

--- src/parsing.py
+++ src/parsing.py
@@ -1,3 +1,8 @@
+import re
+import datetime
+from collections import defaultdict
+
+
 def parsing(file_path):
     kessai_dict_list = []
     # ファイル読み込み
@@ -8,17 +13,23 @@ def parsing(file_path):
     all_kessai_list = raw_text.split("\n\n")[1:]
     for kessai_list in all_kessai_list[1:]:
         kessai_date, kessai_tmp = kessai_list.split("\n", 1)
+        year, month, day = map(int, kessai_date[:-3].split("/"))
+        kessai_date = datetime.date(year, month, day).strftime("%Y-%m-%d")
         oneday_kessai_list = kessai_tmp.split("]")[:-1]
-        # print(kessai_date)
         for one_kessai in oneday_kessai_list:
-            kessai_dict = {"date": kessai_date}
-            kessai_dict["time"], kessai_dict[
+            kessai_dict = defaultdict(str)
+            kessai_dict["date"] = kessai_date[:-3]  # 曜日は不要
+            kessai_time, kessai_dict[
                 "name"], kessai_other = one_kessai.split("\t", 2)
+            hour, minute = map(int, kessai_time.replace("\n", "").split(":"))
+            kessai_dict["date"] = datetime.datetime(year, month, day, hour, minute).isoformat()
             if kessai_other[1:9] == "LINE Pay":
                 kessai_dict["type"], kessai_other = kessai_other[10:].split(
                     " ", 1)
                 kessai_dict["price"], kessai_dict[
                     "state"], *kessai_others = kessai_other.split("\n")
+                kessai_dict["price"] = re.sub(r"\D", "",
+                                              kessai_dict["price"])  # 数字以外を削除
                 for kessai_other in kessai_others:
                     key, *value = kessai_other.split(": ", 1)
                     if value:
@@ -33,7 +44,7 @@ def parsing(file_path):
 
 if __name__ == "__main__":
     print("parsing.py is called as main.")
-    import os    
+    import os
     # outputフォルダがなければ作成
     if not os.path.exists('./output'): os.mkdir('./output')
     # 書き込み
