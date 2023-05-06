4a524d35ee0e119adb4f940de7712a12f2835fce
misonikomipan <yusaku1107@icloud.com>
2023-05-06 11:39:24 +0900

    [add] main

---
+++ main.py
@@ -0,0 +1,27 @@
+import sys
+import json
+from copy import copy
+sys.path.append('./src')
+from parsing import parsing
+from dictlist_to_csv import dictlist_to_csv
+from post_to_notion import post_to_notion
+
+
+def main(input_file_path, csv_file_path):
+    page_format = json.load(open('./data/page_format.json', 'r', encoding='utf-8'))
+    page_format["Payment"]["select"]["name"] = "LINE Pay"
+    dict_list = parsing(input_file_path)
+    
+    for kessai_dict in dict_list:
+        page = copy(page_format)
+        page["Title"]["title"][0]["text"]["content"] = kessai_dict["加盟店"]
+        page["Date"]["date"]["start"] = kessai_dict["date"]
+        page["Date"]["date"]["end"] = None
+        page["Fee"]["number"] = int(kessai_dict["price"])
+        page["Type"]["select"]["name"] = kessai_dict["type"]
+        page["Detail"]["rich_text"][0]["text"]["content"] = ""
+        post_to_notion(page, True)
+
+
+main(input_file_path='./data/data.txt',
+     csv_file_path='./output/sample_output.csv')
